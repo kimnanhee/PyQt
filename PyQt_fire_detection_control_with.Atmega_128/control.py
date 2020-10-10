33a2a15852b91d8a4820cf3380c6e58dcd2dbeb8
kimnanhee <kimnanhee0225@gmail.com>
2020-10-10 23:59:56 +0900

    [modify] radio Btn

--- PyQt_fire_detection_control_with.Atmega_128/control.py
+++ PyQt_fire_detection_control_with.Atmega_128/control.py
@@ -33,12 +33,21 @@ def relay_control(): # relay 버튼이 눌리면 메시지 전송
 	message = ''.join(['r', 'e', 'l', 'a'])
 	ser.write(bytes(message.encode()))
 
+def auto_control():
+	print("hello")
+	
+def manual_control(self):
+	if self.radioButton_manual.checkState() == Qt.Checked():
+		print("auto check")
+	else:
+		print("auto check ??")
+	
 def signals(self): # 각 버튼이 눌렸을 때 함수 호출
 	self.pushButton_fan.clicked.connect(fan_control)
 	self.pushButton_servo.clicked.connect(servo_control)
 	self.pushButton_relay.clicked.connect(relay_control)
-	self.checkBox_auto.isChecked.connect(auto_control)
-	self.checkBox_manual.isChecked.connect(manual_control)
+	self.radioButton_auto.stateChanged.connect(auto_control)
+	self.radioButton_manual.stateChanged.connect(manual_control)
 
 
 Ui_MainWindow.signals = signals
@@ -49,6 +58,7 @@ if __name__ == "__main__":
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
+    ui.signals()
 
     th = threading.Thread(target=read_thread, args=(ui,)) # 스레드 설정, read_thread함수에 인자로 ui를 넘겨준다
     th.daemon = True;
